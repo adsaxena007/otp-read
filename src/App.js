e18da0ebe96adb400274cab6f779da4f158a097b
“B0242572” <aditya1.saxena@airtel.com>
2023-08-21 14:39:06 +0530

    sa

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 import { useEffect, useState } from 'react';
 function App() {
 
-  const [otp, setOtp] = useState("");
-
+  const [otp, setOtp] = useState();
+  const [data, setData] = useState("sad");
   useEffect(() => {
     const ac = new AbortController();
     navigator.credentials
@@ -13,15 +13,18 @@ function App() {
       })
       .then((otp) => {
         setOtp(otp.code)
+        setData(JSON.stringify(otp))
       })
       .catch((err) => {
+        setData(err.message);
         console.error(err);
       });
   }, [])
 
   return (
     <div className="App">
-      <>{`otp is = ${otp}`}</>
+      <div>{`otp is = ${otp}`}</div>
+      <div>{`data is = ${data}`}</div>
     </div>
   );
 }
