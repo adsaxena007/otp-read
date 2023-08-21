aed50bd40dc9525796dd9eeae79be468447c4421
“B0242572” <aditya1.saxena@airtel.com>
2023-08-21 12:50:54 +0530

    ads

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
-import { useState } from 'react';
-import { useReadOTP } from "react-read-otp";
+import { useEffect, useState } from 'react';
 function App() {
 
   const [otp, setOtp] = useState("");
-  useReadOTP(setOtp);
+
+  useEffect(() => {
+    const ac = new AbortController();
+    navigator.credentials
+      .get({
+        otp: { transport: ["sms"] },
+        signal: ac.signal,
+      })
+      .then((otp) => {
+        setOtp(otp.code)
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, [])
+
   return (
     <div className="App">
       <>{`otp is = ${otp}`}</>
