5a4faa8e967c45eb04380fb55c471642de696d87
“B0242572” <aditya1.saxena@airtel.com>
2023-09-24 12:45:12 +0530

    camera 2

--- src/App.js
+++ src/App.js
@@ -1,33 +1,56 @@
 import './App.css';
-import { useEffect, useRef } from 'react';
+import { useCallback, useRef } from 'react';
+import Webcam from "react-webcam";
+
 function App() {
 
-  const videoRef = useRef();
+  // const videoRef = useRef();
+  const webcamRef = useRef(null);
+  const capture = useCallback(
+    () => {
+      const imageSrc = webcamRef.current.getScreenshot();
+    },
+    [webcamRef]
+  );
+  // const getVideo = () => {
+  //   navigator.mediaDevices.getUserMedia({
+  //     video: { width: 1920, height: 1080 }
+  //   }).then((stream) => {
+  //     let video = videoRef.current;
+  //     video.srcObject = stream;
+  //     video.play().then(() => {
+  //     }).catch((err) => {
+  //       console.log(err);
+  //     });
+  //   }).catch((err) => {
+  //     console.log(err);
+  //   });
+  // }
 
-  const getVideo = () => {
-    navigator.mediaDevices.getUserMedia({
-      video: { width: 1920, height: 1080 }
-    }).then((stream) => {
-      let video = videoRef.current;
-      video.srcObject = stream;
-      video.play().then(() => {
-      }).catch((err) => {
-        console.log(err);
-      });
-    }).catch((err) => {
-      console.log(err);
-    });
-  }
-
-  useEffect(() => {
-    getVideo();
-  }, [videoRef])
-
+  // useEffect(() => {
+  //   getVideo();
+  // }, [videoRef])
+  const videoConstraints = {
+    width: 1280,
+    height: 720,
+    facingMode: "user"
+  };
   return (
     <div className="App">
       <div>
-        <video ref={videoRef}></video>
-        <button></button>
+        {/* <video ref={videoRef}></video>
+        <button></button> */}
+        <>
+          <Webcam
+            audio={false}
+            height={720}
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+            width={1280}
+            videoConstraints={videoConstraints}
+          />
+          <button onClick={capture}>Capture photo</button>
+        </>
       </div>
     </div>
   );
